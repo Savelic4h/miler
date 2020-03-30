a22b1174d26e6131784428a957479a030ae15a17
Savelic4h <gsavelich@mail.ru>
2020-03-30 20:52:20 +0700

    added mnoho

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,18 @@
 $(function () {
-
-
+  $('.slider__big').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider__thumbs'
+  });
+  $('.slider__thumbs').slick({
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    asNavFor: '.slider__big',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
+  new WOW().init();
 });
